bac35eeaf2336fba0fced21f85819fae29af12e1
Ayush_Anjay <ayush2310125@akgec.ac.in>
2024-10-06 01:41:11 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:authenticiation_app/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+  apiKey: "AIzaSyCilOEZmeJ6dYvjcldaNqOtf78Q4ZUcaoY",
+  appId: "1:47749362977:web:d5574d13425619d62b3a60",
+  messagingSenderId: "47749362977",
+  projectId: "authapp1-72314",
+      
+    
+      
+      
+    ),
+  );
   runApp(const MyApp()); 
 }
 
